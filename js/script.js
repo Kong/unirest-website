b2a8c3df99647ddef6f6ad16813cd4676fd32750
Montana Flynn <montana@montanaflynn.me>
2013-04-23 16:51:07 -0700

    Template

---
+++ js/script.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+	
+	/* Scroll to section */
+	hash = window.location.hash;
+	$('section.selected').removeClass('selected');
+	if (hash) $('section' + hash).addClass('selected');
+	$.scrollTo($('section' + hash), 1000, { 
+		onAfter: function () {		
+			window.location.hash = hash;
+			enableScroll = true;
+		},
+		easing: 'easeOutQuad'
+	});
+
+
+	/* Background */
+	$.supersized({
+	
+		// Functionality
+		slide_interval          :   4000,		// Length between transitions
+		transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+		transition_speed		:	2000,		// Speed of transition
+
+		horizontal_center		:	1,
+		min_width				:	$(window).width(),
+												   
+		// Slideshow Images							
+		slides 					:  	[
+			{image : 'http://farm7.staticflickr.com/6224/6240022647_41233835f4_b.jpg', thumb: ''},
+			{image : 'http://farm2.staticflickr.com/1256/4728043610_f7889a00a8_b.jpg', thumb: ''},
+			{image : 'http://farm6.staticflickr.com/5252/5453113651_703fc669bc_o.jpg', thumb: ''}
+		]
+		
+	});
+
+	/* Navigation */
+	$('nav.main a').click(function () {
+		
+		/* Select menu item */
+		$('nav.main a').removeClass('selected');
+		$(this).addClass('selected');
+		
+		/* Scroll to section */
+		hash = $(this).attr('href');
+		$('section.selected').removeClass('selected');
+		$('section' + hash).addClass('selected');
+		$.scrollTo($('section' + hash), 1000, { 
+			onAfter: function () {		
+				window.location.hash = hash;
+				enableScroll = true;
+			},
+			easing: 'easeOutQuad'
+		});
+		return false;
+	});
+
+	$('.back').click(function () {
+
+		/* Select menu item */
+		$('nav.main a').removeClass('selected');
+		$(this).addClass('selected');
+
+		/* Scroll to section */
+		hash = $(this).attr('href');
+		$.scrollTo($('section' + hash), 1000, { 
+			onAfter: function () {		
+				window.location.hash = hash;
+				enableScroll = true;
+				$('section.selected').removeClass('selected');
+				$('section' + hash).addClass('selected');
+			},
+			easing: 'easeOutQuad'
+		});
+	});
+
+
+	/* Prevent default (don't refresh) */
+	$('nav.main a, .back, article a.top,').click(function (e) {
+		e.preventDefault();
+	});
+
+});
