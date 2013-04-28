a34efb6939bbdaf94fdecc771e13c1fbd89b5d16
Montana Flynn <montana@montanaflynn.me>
2013-04-28 04:24:44 -0700

    GitHub buttons and JS refactor

--- js/script.js
+++ js/script.js
@@ -1,49 +1,32 @@
 $(document).ready(function () {
 	
-	/* Scroll to section */
-	hash = window.location.hash;
-	$('section.selected').removeClass('selected');
-	if (hash) $('section' + hash).addClass('selected');
-	$.scrollTo($('section' + hash), 1000, { 
-		onAfter: function () {		
-			window.location.hash = hash;
-			enableScroll = true;
-			$('nav.main a[href="'+ hash + '"]').addClass('selected');
-		},
-		easing: 'easeOutQuad'
-	});
+	var hash = window.location.hash;
 
+	if (hash) {
+		doThings(hash);
+	}
 
-	/* Background */
-	$.supersized({
-	
-		// Functionality
-		slide_interval          :   4000,		// Length between transitions
-		transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-		transition_speed		:	2000,		// Speed of transition
+    window.onhashchange = function(){
+		hash = window.location.hash;
+		doThings(hash);
+	}
 
-		horizontal_center		:	1,
-		min_width				:	$(window).width(),
-												   
-		// Slideshow Images							
-		slides 					:  	[
-			{image : 'http://farm6.staticflickr.com/5252/5453113651_703fc669bc_o.jpg', thumb: ''}
-		]
-		
-	});
+	/* Logic */
+	function doThings(hash) {
+		console.log(hash);
+		var lang = hash.substr(1);
 
-	/* Navigation */
-	$('nav.main a').click(function () {
-		
-		/* Scroll to section */
-		hash = $(this).attr('href');
+		/* Github Buttons */
+		$('#social').hide();
+		$('header .github div').hide();
+		$('header .github-'+lang).fadeIn();
+
 		$('section.selected').removeClass('selected');
 		$('section' + hash).addClass('selected');
 		$('nav.main a').removeClass('selected');
 		$('nav.main a[href="'+ hash + '"]').addClass('selected');
-		$.scrollTo($('section' + hash), 1000, { 
+		$.scrollTo($('section' + hash), 500, { 
 			onAfter: function () {		
-				window.location.hash = hash;
 				enableScroll = true;
 				analytics.track('Selected Language', {
 				    language        : hash,
@@ -51,32 +34,15 @@ $(document).ready(function () {
 			},
 			easing: 'easeOutQuad'
 		});
-		return false;
-	});
+	}
 
-	$('.back').click(function () {
-
-		/* Select menu item */
-		$('nav.main a').removeClass('selected');
-		$(this).addClass('selected');
-
-		/* Scroll to section */
-		hash = $(this).attr('href');
-		$.scrollTo($('section' + hash), 1000, { 
-			onAfter: function () {		
-				window.location.hash = hash;
-				enableScroll = true;
-				$('section.selected').removeClass('selected');
-				$('section' + hash).addClass('selected');
-			},
-			easing: 'easeOutQuad'
-		});
-	});
-
-
-	/* Prevent default (don't refresh) */
-	$('nav.main a, .back, article a.top,').click(function (e) {
-		e.preventDefault();
+	/* Background */
+	$.supersized({
+		horizontal_center		:	1,
+		min_width				:	$(window).width(),
+		slides 					:  	[
+			{image : 'http://farm6.staticflickr.com/5252/5453113651_703fc669bc_o.jpg', thumb: ''}
+		]
 	});
 
 });
